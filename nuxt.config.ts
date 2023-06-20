f9c689d7afa4dfad7d5b95bdcd9436e38e278882
yutzu <tsengm6h6@gmail.com>
2023-06-21 02:54:43 +0800

    change favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,10 +7,7 @@ export default defineNuxtConfig({
     pageTransition: { name: 'page', mode: 'out-in' },
     head: {
       link: [
-        { rel: 'apple-touch-icon', sizes: '180x180', href: 'apple-touch-icon.png' },
-        { rel: 'icon', type: 'image/png', sizes: '32x32', href: 'favicon-32x32.png' },
-        { rel: 'icon', type: 'image/png', sizes: '16x16', href: 'favicon-16x16.png' },
-        { rel: 'manifest', href: 'site.webmanifest' },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
         {
           rel: 'stylesheet',
           href: 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css',
