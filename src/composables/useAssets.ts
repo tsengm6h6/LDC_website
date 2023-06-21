081357ad8dee2b06f4f32a005349e1651a523a6f
Angelo <ben7152000@gmail.com>
2023-06-22 00:58:14 +0800

    fixed: 動態圖片修正

---
+++ src/composables/useAssets.ts
@@ -0,0 +1,26 @@
+import { filename } from 'pathe/utils'
+export default function useAssets(path: string) {
+  let assets
+  if (/(\.svg)$/i.exec(path)) assets = import.meta.glob('~/assets/image/**/*.svg', { eager: true })
+  else if (/(\.png)$/i.exec(path))
+    assets = import.meta.glob('~/assets/image/**/*.png', { eager: true })
+  else assets = import.meta.glob('~/assets/image/**/*.jpg', { eager: true })
+  const fileName = filename(path)
+  const images = Object.fromEntries(
+    Object.entries(assets).map(([key, value]) => [
+      filename(key),
+      (value as Record<string, any>).default,
+    ])
+  )
+  return images[fileName]
+}
+
+export const getImages = () => {
+  const assets = import.meta.glob('~/assets/image/**/*.(svg|png|jpg|jpeg)', { eager: true })
+  return Object.fromEntries(
+    Object.entries(assets).map(([key, value]) => [
+      filename(key),
+      (value as Record<string, any>).default,
+    ])
+  )
+}
