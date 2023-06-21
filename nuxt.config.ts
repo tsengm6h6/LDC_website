3d562f96c01c7ca8502ff2c4456830e9fc750828
Angelo <ben7152000@gmail.com>
2023-06-21 19:40:27 +0800

    fixed: nuxt config 配置調整

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,10 @@ export default defineNuxtConfig({
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
     head: {
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
         {
