142300b719acda8d78943671dfc27dba7f21d73b
yutzu <tsengm6h6@gmail.com>
2023-06-23 18:11:13 +0800

    style: logo 加上高度

--- src/components/AppLogo.vue
+++ src/components/AppLogo.vue
@@ -7,5 +7,5 @@
     fit="contain"
     format="png"
     quality="90"
-    class="w-14 object-contain object-center md:w-20" />
+    class="h-14 w-14 object-contain object-center md:h-20 md:w-20" />
 </template>
